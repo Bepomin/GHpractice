ccc9886da0cbae0e9a95e39f62b6a60dedcfa421
Bepomin <kalkulza@naver.com>
2021-12-17 09:33:09 +0900

    this is commit message

---
+++ src/githubPretice/org/testClass.java
@@ -0,0 +1,11 @@
+package githubPretice.org;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello git");
+		//gitHub Pratice
+	}
+
+}
